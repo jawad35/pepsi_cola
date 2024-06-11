aa4b3d0f578c2f0cfea94cac36d747f382ac663d
jawad35 <jaqimughal@gmail.com>
2024-06-11 18:25:29 +0500

    new work done

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -28,9 +28,4 @@ router.route('/admin/product/:id')
     .put(isAuthenticatedUser, authorizeRoles('admin'), updateProduct)
     .delete(isAuthenticatedUser, authorizeRoles('admin'), deleteProduct);
 
-
-router.route('/review').put(isAuthenticatedUser, createProductReview)
-router.route('/reviews').get(isAuthenticatedUser, getProductReviews)
-router.route('/reviews').delete(isAuthenticatedUser, deleteReview)
-
 module.exports = router;
